d94e1bd52a4c886ee56ba5a25afe107f64251ddc
vitor souza <vitordesouzavieira169@gmail.com>
2026-03-31 04:50:21 -0300

    criaçaõ da implementação DAO seller JDBC e crição da factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,7 +14,11 @@ public class Program {
 		Department obj = new Department(1,"books");
 		
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+		
 		System.out.println(obj);
+		
 		System.out.println(seller);
 	}
 
